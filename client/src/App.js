f4788029c26c8789014434a19ddb6f8b9f608ae5
Ethan Freeman <ethanfree23@gmail.com>
2022-04-24 08:36:09 -0500

    navbar setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import NavBar from './Components/NavBar.js'
 function App() {
   return (
     <div className='App'>
-      <div className='sidebar'></div>
-      <div className='main-content'>
+      <div className='sidebar'>
         <NavBar />
+      </div>
+      <div className='main-content'>
+        
       </div>
     </div>
   );
